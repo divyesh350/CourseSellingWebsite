9bb37c31d073bcabe8d850d68b1f587935124e45
divyesh350 <bakaraniyadivyesh94@gmail.com>
2025-01-18 10:14:43 +0530

    first commit

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,9 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    const connection = await mongoose.connect(process.env.MONGO_URI);
+    const connection = await mongoose.connect(process.env.MONGO_URI 
+      || "mongodb+srv://divyesh:divyesh@grocerystore.m7mkj.mongodb.net/?retryWrites=true&w=majority&appName=grocerystore"
+    );
     // console.log(
     //   `MongoDB connected successfully on host: ${connection.connection.host}, database: ${connection.connection.db.databaseName}`
     // );
